057e38d843fed4081892f2a8ac94b2fe6bfc1db1
Devin Cook <devin.cook@CD.local>
2016-11-22 13:57:34 -0800

    adding untracked files

---
+++ classes/ScrollHelper.js
@@ -0,0 +1,36 @@
+class ScrollHelper {
+  static disableScroll = false
+  static scrollTop = 0
+
+  static toggle () {
+    this.disableScroll = !this.disableScroll
+    console.log('toggle')
+    if (this.disableScroll) {
+      this.scrollTop = document.body.scrollTop
+      document.body.style.position = 'relative'
+      document.querySelector('html').classList.add('disable-scroll')
+      document.body.style.top = this.scrollTop * -1 + 'px'
+    } else {
+      document.querySelector('html').classList.remove('disable-scroll')
+      document.body.style.top = '0px'
+      document.body.scrollTop = this.scrollTop
+    }
+  }
+
+  static enable () {
+    this.disableScroll = false
+    document.querySelector('html').classList.remove('disable-scroll')
+    document.body.style.top = '0px'
+    document.body.scrollTop = this.scrollTop
+  }
+
+  static disable () {
+    this.disableScroll = true
+    this.scrollTop = document.body.scrollTop
+    document.body.style.position = 'relative'
+    document.querySelector('html').classList.add('disable-scroll')
+    document.body.style.top = this.scrollTop * -1 + 'px'
+  }
+}
+
+export default ScrollHelper
